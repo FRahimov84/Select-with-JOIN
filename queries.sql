a79e524ac0580ad09263dab4a2a91af81b2a856f
Lambo <Lambo@localhost>
2020-02-05 19:13:34 +0500

    fix: fixed #2, fixed #3

--- queries.sql
+++ queries.sql
@@ -11,73 +11,57 @@
 -- 1
 select t.id, t.sum, t.manager, p.name product
 from (select s.id id, (s.price * s.qty) sum, m.name manager, s.product_id
-      from managers m
-               JOIN sales s on m.id = s.manager_id) t
+      from sales s
+               JOIN managers m on m.id = s.manager_id) t
          join products p on t.product_id = p.id;
 
 -- 2
-select m.name manager, count(s.manager_id) salesCount
+select m.name manager, ifnull(s.salesCount, 0) total
 from managers m
-         LEFT JOIN sales s on m.id = s.manager_id
-group by s.manager_id;
+         LEFT JOIN (select manager_id, count(manager_id) salesCount from sales group by manager_id) s
+                   on m.id = s.manager_id;
 
 -- 3
-select m.name manager, ifnull(sum(s.price * s.qty), 0) salesSum, m.plan
+select m.name manager, ifnull(s.sum, 0) total
 from managers m
-         LEFT JOIN sales s on m.id = s.manager_id
-group by s.manager_id;
+         LEFT JOIN (select manager_id, sum(qty * price) sum from sales group by manager_id) s
+                   on m.id = s.manager_id;
 
 -- 4
-select p.name product, ifnull(sum(s.price * s.qty), 0) salesSum
+select p.name product, ifnull(s.sum, 0) total
 from products p
-         LEFT JOIN sales s on p.id = s.product_id
-group by s.product_id;
+         LEFT JOIN (select product_id, sum(qty * price) sum from sales group by product_id) s
+                   on p.id = s.product_id;
 
 -- 5
-select m.name manager, ifnull(sum(s.price * s.qty), 0) salesSum
+select m.name manager, ifnull(s.sum, 0) salesSum
 from managers m
-         LEFT JOIN sales s on m.id = s.manager_id
-group by s.manager_id
+         LEFT JOIN (select manager_id, sum(qty * price) sum from sales group by manager_id) s on m.id = s.manager_id
 order by salesSum desc
 limit 3;
 
 -- 6
-select p.name product, ifnull(sum(s.qty), 0) totalQty
+select p.name product, ifnull(s.sum, 0) totalQty
 from products p
-         LEFT JOIN sales s on p.id = s.product_id
-group by s.product_id
+         LEFT JOIN (select product_id, sum(qty) sum from sales group by product_id) s on p.id = s.product_id
 order by totalQty desc
 limit 3;
 
 -- 7
-select p.name product, ifnull(sum(s.price * s.qty), 0) totalSum
+select p.name product, ifnull(s.sum, 0) totalSum
 from products p
-         LEFT JOIN sales s on p.id = s.product_id
-group by s.product_id
+         LEFT JOIN (select product_id, sum(price * qty) sum from sales group by product_id) s on p.id = s.product_id
 order by totalSum desc
 limit 3;
 
 -- 8
-select m.name manager, ifnull((sum(s.price * s.qty) * 100.0 / m.plan), 0) percent
+select m.name manager, ifnull((s.sum * 100.0 / m.plan), 0) percent
 from managers m
-         LEFT JOIN sales s on m.id = s.manager_id
-group by s.manager_id;
+         LEFT JOIN (select manager_id, sum(qty * price) sum from sales group by manager_id) s on m.id = s.manager_id;
 
 -- 9
-SELECT ifnull(m.unit, 'boss') Unit,
-       ifnull((
-                  SELECT ss.total
-                  FROM (
-                           SELECT sum(s.qty * s.price)                                         total,
-                                  (SELECT mm.unit FROM managers mm WHERE mm.id = s.manager_id) unit
-                           FROM sales s
-                           WHERE unit = m.unit) ss
-              )
-                  * 100.0 /
-              (
-                  SELECT sum(mm.plan)
-                  FROM managers mm
-                  WHERE mm.unit = m.unit
-              ), 0)           percent
-FROM managers m
-group by Unit;
+select ifnull(m.unit, 'boss') unit, ifnull(sum(s.sum) * 100.0 / sum(m.plan), 0) percent
+from managers m
+         LEFT JOIN (SELECT manager_id, SUM(qty * price) sum FROM sales Group By manager_id) s
+                   on s.manager_id = m.id
+GROUP BY m.unit;
